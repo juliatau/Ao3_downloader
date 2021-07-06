071665d1fc2fa6c5ee2e3d3ecf207991251d92ca
juliatau <blodeuweddtau@gmail.com>
2021-07-06 22:57:43 +0200

    now starting thread

--- downloader.py
+++ downloader.py
@@ -118,7 +118,9 @@ def loadWorkMetadata(works):
     print(f"starting to check you {Fore.CYAN}{len(works)}{Fore.RESET} subsciption")
     for work in works:
         loadedWorks.append(work)
-        threads.append(threading.Thread(target=loadWorkMetadata, args=(work)))
+        newThread = threading.Thread(target=loadWorkMetadata, args=(work))
+        newThread.start()
+        threads.append(newThread)
     for thread in threads:
         thread.join()
     return loadedWorks
